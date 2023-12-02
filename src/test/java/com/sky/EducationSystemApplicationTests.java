93b6dd4861c6434c6d87a61d2c07ca538274185c
luofan <1455827681@qq.com>
2023-12-02 13:01:24 +0800

    项目脚手架

--- src/test/java/com/sky/EducationSystemApplicationTests.java
+++ src/test/java/com/sky/EducationSystemApplicationTests.java
@@ -6,7 +6,7 @@ import com.sky.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-//
+//t
 @SpringBootTest
 class EducationSystemApplicationTests {
     @Autowired
